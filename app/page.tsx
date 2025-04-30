a2fff4c4fd3321e6f6c703b62fc604a9cde26c59
honkai-yan <peter-adams@qq.com>
2025-04-30 11:05:23 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,13 +5,15 @@ export default function Home() {
   return (
     <div className="flex flex-col p-[20px] items-center w-full min-h-full bg-gray-400">
       <div className="w-full max-w-[500px] min-h-10 bg-white/75 shadow-md rounded-xl p-4">
-        <h1 className="pb-5 text-2xl">Welcome to My Linux Study Demo</h1>
-        {systemDetail[1].map((item, idx) => (
-          <div className="flex flex-row items-center">
-            <span className="block w-[120px]">{systemDetail[0][idx]}：</span>
-            <span>{item}</span>
-          </div>
-        ))}
+        <h1 className="pb-5 text-2xl">Linux高可用Web服务器集群 测试应用</h1>
+        <div className="flex flex-col gap-3">
+          {systemDetail[1].map((item, idx) => (
+            <div className="flex flex-row items-center">
+              <span className="block w-[120px]">{systemDetail[0][idx]}：</span>
+              <span>{item}</span>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
